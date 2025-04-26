77f1e47cbb42c8f384be61ff46adebb41253af2e
perevedko <perevedko>
2025-04-26 16:51:16 +0400

    rewrited task 2 implementation to make it easier to read

--- 2/word_splitter.rb
+++ 2/word_splitter.rb
@@ -1,26 +1,27 @@
 class WordSplitter
   def initialize(dictionary)
-    @dictionary = dictionary.to_set
-    @word_lengths = @dictionary.map(&:length).uniq.sort.reverse
+    @dictionary = dictionary.to_set.freeze
+    @word_lengths = @dictionary.map(&:length).uniq.sort.reverse.freeze
   end
 
-  def can_split?(s)
-    n = s.length
-    dp = Array.new(n + 1, false)
-    dp[0] = true
+  def can_split?(string)
+    string_length = string.length
+    helper = Array.new(string_length + 1, false)
+    helper[0] = true
 
-    (1..n).each do |i|
-      @word_lengths.each do |length|
-        next if i < length
+    (1..string_length).each do |i|
+      @word_lengths.each do |word_length|
+        next if i < word_length
 
-        start_index = i - length
-        if dp[start_index] && @dictionary.include?(s[start_index, length])
-          dp[i] = true
-          break
-        end
+        start_index = i - word_length
+        next unless helper[start_index]
+        next unless @dictionary.include?(string[start_index, word_length])
+
+        helper[i] = true
+        break
       end
     end
 
-    dp[n]
+    helper[string_length]
   end
 end
